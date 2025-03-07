e94d9c8eafc8870a1729fa60a80301f8cfe4e83f
Thiago Franco de Moraes <totonixsame@gmail.com>
2025-03-06 21:41:29 -0300

    Added early stop

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ import datetime
 from typing import Callable
 
 import torch
-from torch import nn, optim
+from torch import minimum, mode, nn, optim
 from torch.optim.lr_scheduler import CosineAnnealingLR, ReduceLROnPlateau
 from torch.utils.data import DataLoader, random_split
 from torch.utils.tensorboard import SummaryWriter
@@ -15,7 +15,22 @@ from model import ResNet18, ResNet34, ResNet50, ResNet101
 
 LossFunctionType = Callable[[torch.Tensor, torch.Tensor], torch.Tensor]
 
+MODELS = {
+    "resnet18": ResNet18,
+    "resnet34": ResNet34,
+    "resnet50": ResNet50,
+    "resnet101": ResNet101,
+}
+
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument(
+    "-m",
+    "--model",
+    choices=list(MODELS.keys()),
+    metavar="MODEL",
+    default=list(MODELS.keys())[0],
+    help="Models: {%(choices)s}",
+)
 parser.add_argument(
     "-e",
     "--epochs",
@@ -36,6 +51,15 @@ parser.add_argument(
     metavar="LR",
     help="Learning rate",
     dest="lr",
+)
+
+parser.add_argument(
+    "--early-stop",
+    default=5,
+    type=int,
+    metavar="N",
+    help="Number of epochs of no improvement to early stop. If 0 then early-stop is not activated.",
+    dest="early_stop",
 )
 
 args, _ = parser.parse_known_args()
@@ -97,10 +121,49 @@ def _evaluate(
     return avg_validation_loss, avg_validation_accuracy
 
 
+class EarlyStopper:
+    def __init__(self, patience: int = 5, min_delta: float = 0.0, mode: str="min"):
+        """
+        patience: number of epochs to wait before stopping. if 0 deactivate early stop
+        min_delta: minimun change to qualify as improvement
+        mode: 'min' (for loss) or 'max' (for accuracy)
+        """
+
+        self.patience = patience
+        self.min_delta = min_delta
+        self.mode = mode
+        self.counter = 0
+        self.best_metric = None
+
+    def __call__(self, metric: float) -> bool:
+        if self.patience == 0:
+            return False
+
+        if self.best_metric is None:
+            self.best_metric = metric
+            return False
+
+        if self.mode == 'min':
+            improved = (metric < self.best_metric - self.min_delta)
+        else:
+            improved = (metric > self.best_metric + self.min_delta)
+
+        if improved:
+            self.best_metric = metric
+            self.counter = 0
+        else:
+            self.counter += 1
+            if self.counter >= self.patience:
+                return True
+        return False
+
+
 def train(
+    model: nn.Module,
     batch_size: int = 128,
     epochs: int = 10,
     learning_rate: float = 0.001,
+    patience: int = 10,
     device: torch.device = torch.device(
         "cuda:0" if torch.cuda.is_available() else "cpu"
     ),
@@ -147,18 +210,22 @@ def train(
     loader_validation = DataLoader(dataset_val, batch_size=batch_size, shuffle=False)
     loader_test = DataLoader(dataset_test, batch_size=batch_size, shuffle=False)
 
-    model = ResNet50()
     model.to(device)
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr=learning_rate, weight_decay=1e-4)
-    scheduler = ReduceLROnPlateau(optimizer, "max", patience=3)
+    optimizer = optim.SGD(
+        model.parameters(), lr=learning_rate, momentum=0.9, weight_decay=5e-4
+    )
+    scheduler = CosineAnnealingLR(optimizer, T_max=epochs)
 
     metric_fn = Accuracy(task="multiclass", num_classes=10).to(device)
 
-    best_val_loss = float("inf")
+    best_val_accuracy = float("inf")
     best_model_path = "best_model.pth"
-    writer = SummaryWriter(log_dir=f"./runs/{datetime.datetime.now():%Y%m%d_%H%M%S}")
+    writer = SummaryWriter(
+        log_dir=f"./runs/{model.__class__.__name__}.{optimizer.__class__.__name__}_{learning_rate}.{scheduler.__class__.__name__}"
+    )
     writer.add_graph(model, torch.randn(1, 3, 32, 32).to(device))
+    early_stopper = EarlyStopper(patience=10, mode='max')
     for epoch in trange(epochs):
         avg_train_loss, avg_train_accuracy = _train(
             loader_train, model, criterion, metric_fn, optimizer, device
@@ -166,7 +233,7 @@ def train(
         avg_val_loss, avg_val_accuracy = _evaluate(
             loader_validation, model, criterion, metric_fn, device
         )
-        scheduler.step(avg_val_accuracy)
+        scheduler.step()
 
         writer.add_scalar("Training loss", avg_train_loss, epoch)
         writer.add_scalar("Training accuracy", avg_train_accuracy, epoch)
@@ -177,9 +244,13 @@ def train(
             f"Epoch {epoch + 1}/{epochs} | train loss: {avg_train_loss:.6f} | train acc: {avg_train_accuracy:.6f} | val loss: {avg_val_loss:.6f} | val acc: {avg_val_accuracy:.6f}"
         )
 
-        if avg_val_loss < best_val_loss:
-            best_val_loss = avg_val_loss
+        if avg_val_accuracy > best_val_accuracy:
+            best_val_accuracy = avg_val_accuracy
             torch.save(model.state_dict(), best_model_path)
+
+        if early_stopper(avg_val_accuracy):
+            print(f'Early stop at epoch {epoch + 1}')
+            break
 
     # Loading best weights for test
     model.load_state_dict(torch.load(best_model_path))
@@ -195,4 +266,5 @@ def train(
 
 
 if __name__ == "__main__":
-    train(epochs=args.epochs, batch_size=args.batch_size, learning_rate=args.lr)
+    model = MODELS[args.model]()
+    train(model, epochs=args.epochs, batch_size=args.batch_size, learning_rate=args.lr, patience=args.early_stop)
